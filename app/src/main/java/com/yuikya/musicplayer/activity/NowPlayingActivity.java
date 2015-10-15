ec1dfb285f159f1ab994686c752ab7ac2019f73c
yuikya <yuikyamail@gmail.com>
2015-10-15 15:52:16 +0800

    first commit

--- app/src/main/java/com/yuikya/musicplayer/activity/NowPlayingActivity.java
+++ app/src/main/java/com/yuikya/musicplayer/activity/NowPlayingActivity.java
@@ -1,12 +1,16 @@
 package com.yuikya.musicplayer.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.yuikya.musicplayer.R;
 
@@ -18,7 +22,7 @@ public class NowPlayingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.nowplaying);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
@@ -30,6 +34,17 @@ public class NowPlayingActivity extends AppCompatActivity {
             window.setStatusBarColor(Color.TRANSPARENT);
             window.setNavigationBarColor(Color.TRANSPARENT);
         }
-        setContentView(R.layout.nowplaying);
+        final LinearLayout top = (LinearLayout) findViewById(R.id.nowplaying_top);
+
+        //提取主色调
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                Palette.Swatch swatch = palette.getVibrantSwatch();
+                //setup color
+                top.setBackgroundColor(swatch.getRgb());
+            }
+        });
     }
 }
